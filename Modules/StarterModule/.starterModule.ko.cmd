f5a2c1d51726591c6549710c538709b1c7b33828
Vop <eryk1994@yahoo.ie>
2017-05-02 15:07:01 +0800

    Fixed files

--- Modules/StarterModule/.starterModule.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/vop/Desktop/Beihang/Security/Modules/StarterModule/starterModule.ko := ld -r -m elf_i386 -T ./scripts/module-common.lds --build-id  -o /home/vop/Desktop/Beihang/Security/Modules/StarterModule/starterModule.ko /home/vop/Desktop/Beihang/Security/Modules/StarterModule/starterModule.o /home/vop/Desktop/Beihang/Security/Modules/StarterModule/starterModule.mod.o
