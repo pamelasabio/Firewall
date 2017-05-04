d1a5923517bb6f9c95542dae7c4d8053fc956f8d
Vop <eryk1994@yahoo.ie>
2017-05-04 17:42:52 +0800

    Added UDP

--- Modules/NetfilterModule/netfilterModule.c
+++ Modules/NetfilterModule/netfilterModule.c
@@ -1,5 +1,5 @@
-#define PROTOCOL_PORT_TCP 6
-#define PROTOCOL_PORT_UDP 17
+#define PROTOCOL_TCP 6
+#define PROTOCOL_UDP 17
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/netdevice.h>
@@ -17,38 +17,47 @@ static struct nf_hook_ops nfho;		       // Struct holding set of hook function o
 static unsigned char *ip = "\xC0\xA8\x00\x01"; // Ip in network byte order (192.168.0.1);
 static char *interface = "lo";                 // Loop-back interface which will be blocked
 unsigned char *telnet_port = "x00\x17";	       // The telnet port
+struct udphdr *udp_header;
+struct sk_buff *sk_buffer;
+struct iphdr *ip_header;
 
 // Hook function that is assigned to nfho.hook
 unsigned int hook_func(void *priv, struct sk_buff *skb, const struct nf_hook_state *state)
 {
-	struct sk_buff *socket_buff;	// Socket kernel buffer
-	struct udphdr *upd_header;	// Struct for UDP header
+	//struct udphdr *udp_header;	// Struct for UDP header
 	struct tcphdr *tcp_header; 	// Struct for TCP header
-	struct iphdr *ip_header;	// Struct for IP header  (ip_header->saddr || ip_header->daddr)
+	//struct iphdr *ip_header;	// Struct for IP header  (ip_header->saddr || ip_header->daddr)
 	struct httphdr *http_header;	// Struct for HTTP header
 	struct smtphdr *smtp_header;	// Struct for SMTP header
-	// TODO: Figure out a way to access net_device struct in this version of kernel
+
 	// Check if we are dealing with loop-back interface is so then drop it.
-  	//if(strcmp(in->name,interface) == 0){
-	//	return NF_DROP;
-  	//}
-	// Assign *skb to socket_buff
-	socket_buff = skb;
-	//if(!(socket_buff)) { return NF_ACCEPT; } // Validate socket_buff
-	//if(!(socket_buff->nh.iph)) { return NF_ACCEPT; }; // Validate IP packet
-	//if(socket_buff->nh.ipg->saddr == *(unsigned int*)ip){return NF_DROP;} // Compare ip addresses
-	
-	// TODO COMPARE WITH UDP
-	 
-	*ip_header = (struct iphdr *)skb_network_header(skb); // Assign the ip_header
-	if (ip_header->protocol == PROTOCOL_PORT_TCP) // Check if it is TCP protocol
+  	if(strcmp(state->in->name,interface) == 0){
+		return NF_DROP;
+  	}
+	sk_buffer = skb;
+
+	if(!(sk_buffer)) { return NF_ACCEPT; } // Validate socket_buff
+	ip_header = (struct iphdr *)skb_network_header(sk_buffer); // Assign the ip_header
+	if(!(ip_header)){return NF_ACCEPT;} // Validate IP Packet
+	if(ip_header->saddr == *(unsigned int *)ip){return NF_DROP;} // Compare IP  
+
+	if (ip_header->protocol == PROTOCOL_UDP){
+		printk(KERN_INFO "UDP Packet\n");
+		//udp_header = (struct udphdr *)(skb_transport_header(skb) + 20);
+		//printk(KERN_INFO "SOURCE PORT %u\n",udp_header->source);
+		// or
+		udp_header = (struct udphdr *)(sk_buffer->data + (ip_header->ihl *4));
+		//printk(KERN_INFO "SOURCE 2 PORT %u\n",udp_header->source);
+		if((udp_header->dest) == *(unsigned short*)telnet_port){ return NF_DROP;}
+	}else if (ip_header->protocol == PROTOCOL_TCP) // Check if it is TCP protocol
 	{
 		printk(KERN_INFO "TCP Packet\n");
 		tcp_header = (struct tcphdr *)(skb_transport_header(skb)+20); //Note: +20 is only for incoming packets
 		printk(KERN_INFO "Source Port: %u\n", tcp_header->source);
  		printk(KERN_INFO "Dest Port: %u\n", tcp_header->dest);
+		
 	}
-  
+ 
   return NF_ACCEPT;                                                                   //accept the packet
 }
 
