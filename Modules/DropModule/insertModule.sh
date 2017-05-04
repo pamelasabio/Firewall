b23739fc700154b227967e593c2b23f38af84543
Pamela Sabio <pamelalouisse@yahoo.com>
2017-05-04 18:55:12 +0900

    Deleted files

--- Modules/DropModule/insertModule.sh
+++ Modules/DropModule/insertModule.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 # Inserts module to the kernel
-#insmod drop.ko
-insmod firewall.ko
+insmod drop.ko
 # Will display init function
 dmesg | tail -1
 
 # Removes the module from the kernel
 #rmmod drop.ko
-#rmmod firewall.ko
 # Will display exit function
 #dmesg | tail -1
